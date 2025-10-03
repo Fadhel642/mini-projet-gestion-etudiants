ba54600d5ae89ddef6f7f476a036065cdd5ec4e3
Fadhel642 <213682937+Fadhel642@users.noreply.github.com>
2025-10-03 03:58:42 +0200

    feat: add function to insert new student into database

--- src/gestion_etudiants.py
+++ src/gestion_etudiants.py
@@ -1,1 +1,12 @@
 # script principal
+def ajouter_etudiant(etudiants, id_, nom, prenom, notes):
+    if id_ in etudiants:
+        print("❌ ID déjà utilisé")
+        return
+    etudiants[id_] = {
+        "id": id_,
+        "nom": nom,
+        "prenom": prenom,
+        "notes": notes
+    }
+    print("✅ Étudiant ajouté :", nom, prenom)
